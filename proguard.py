daf20b614c66d00f2429041bf0feaa0568ecd86e
xqgtiti <xqgtiti@163.com>
2017-12-13 21:38:37 +0800

    proguard

--- proguard.py
+++ proguard.py
@@ -1,6 +1,25 @@
 # -*- coding:utf-8 -*- -
+import sys
+import os
+import shlex, subprocess
 
-def shrink():
-    a = 1
+from global_path import *
+reload(sys)
+sys.setdefaultencoding('utf-8')
+def shrink(apk_name):
+    f = open('F:\PROGUARD_WORK_SPACE\TOTAL_APK_ROOT_GOOGLE\dir_%s//android_single_dex.pro'%apk_name,'a')
+
+    os.chdir(PROGUARD_WORK_SPACE_WIN + 'dir_' + apk_name)
+    comand_line = 'java -jar "D:\workspace\\big_program\\tool\proguard5.3.3\lib\proguard.jar" @android_single_dex.pro'
+    os.system(comand_line)
+    """
+    print comand_line
+    proguard_sub = subprocess.Popen(shlex.split(comand_line), stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                    shell=True)
+    proguard_sub.wait()
+    error, out = proguard_sub.communicate()
+    print error
+    print out
+    """
 if __name__ == '__main__':
-    shrink()
+    shrink('com.jonathanrobins.pepe_snap.apk')
